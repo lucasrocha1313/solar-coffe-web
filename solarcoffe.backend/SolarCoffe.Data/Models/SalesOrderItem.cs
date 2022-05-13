9a81d6662462b96474539fd54aa8210967b2c0da
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2022-05-12 23:30:54 -0300

    add entity models

---
+++ solarcoffe.backend/SolarCoffe.Data/Models/SalesOrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarCoffe.Data.Models
+{
+    public class SalesOrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+    }
+}
