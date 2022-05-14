f5bdf39aed5a9272a140ad7a91d853867ef03a53
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2022-05-14 14:18:24 -0300

    Product Controller

---
+++ solarcoffe.backend/SolarCoffe.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using SolarCoffe.Services.Product.Interfaces;
+
+namespace SolarCoffe.Web.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet("/api/product")]
+        public ActionResult GetProduct()
+        {
+            _logger.LogInformation("Getting all products");
+            _productService.GetAllProducts();
+            return Ok("");
+        }
+    }
+}
