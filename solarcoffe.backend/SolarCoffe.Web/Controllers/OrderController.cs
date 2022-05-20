0621a2d6d1ace22c76067a3cd5066fbc34acd583
lucas rocha <lucasrocha1313@gmail.com>
2022-05-20 13:53:10 -0300

    order controller

---
+++ solarcoffe.backend/SolarCoffe.Web/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffe.Data.Models;
+using SolarCoffe.Services.Customer.Interfaces;
+using SolarCoffe.Services.Order.Interfaces;
+using SolarCoffe.Web.Dtos;
+
+namespace SolarCoffe.Web.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderService _orderService;
+        private readonly ICostumerService _customerService;
+        private readonly IMapper _mapper;
+
+        public OrderController(ILogger<OrderController> logger, IOrderService orderService, ICostumerService customerService, IMapper mapper)
+        {
+            _logger = logger;
+            _orderService = orderService;
+            _customerService = customerService;
+            _mapper = mapper;
+        }
+
+        [HttpPost("/api/invoice")]
+        public ActionResult GenerateNewOrder([FromBody]InvoiceDto invoice) {
+            _logger.LogInformation("Generating invoice");
+            var order = _mapper.Map<SalesOrder>(invoice);
+            order.Customer = _customerService.GetById(invoice.CustomerId);
+            _orderService.GenerateOpenOrder(order);
+            return Ok();
+        }
+    }
+}
