0e8c9a456616611fc48401af3b59725835606db5
lucas rocha <lucasrocha1313@gmail.com>
2022-05-20 15:55:05 -0300

    update size of collumn Postal Code

---
+++ solarcoffe.backend/SolarCoffe.Data/Migrations/20220520174700_UpdateSizePostalcode.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarCoffe.Data.Migrations
+{
+    public partial class UpdateSizePostalcode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "CustomerAddresses",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2)",
+                oldMaxLength: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "CustomerAddresses",
+                type: "character varying(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+        }
+    }
+}
