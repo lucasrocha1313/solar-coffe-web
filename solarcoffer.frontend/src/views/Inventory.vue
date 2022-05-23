827e709db05926e97438270ccf4370ab9e803cd3
lucas rocha <lucasrocha1313@gmail.com>
2022-05-23 14:48:34 -0300

    inventory view

---
+++ solarcoffer.frontend/src/views/Inventory.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="inventory-container">
+        <h1 id="invetory-title">
+            Inventory Dashboard
+        </h1>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue"
+
+    export default defineComponent({
+        name: 'Inventory',
+        components:{},
+        
+    })
+</script>
+
+<style scoped lang="scss">
+</style>
