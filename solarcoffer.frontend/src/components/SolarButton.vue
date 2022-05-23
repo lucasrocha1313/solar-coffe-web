4f4ca1bfd91c58e7785240500015abd38da234e8
lucas rocha <lucasrocha1313@gmail.com>
2022-05-23 14:48:13 -0300

    component for buttons

---
+++ solarcoffer.frontend/src/components/SolarButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="btn-link">
+        <button @click="visitRoute">
+            <slot></slot>
+        </button>
+    </div>
+    
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "SolarButton",
+    components:{},
+    props: {
+        link: String
+    },
+    methods: {
+        visitRoute(): void{
+        if(this.link)
+            this.$router.push(this.link)
+    }
+    }
+    
+})
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
