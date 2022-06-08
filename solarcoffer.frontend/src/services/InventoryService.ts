68fc1234a64307f716acb385288d74f1928397d3
lucas rocha <lucasrocha1313@gmail.com>
2022-06-07 21:11:06 -0300

    inventory service

---
+++ solarcoffer.frontend/src/services/InventoryService.ts
@@ -0,0 +1,21 @@
+import { IProductInventory } from "@/types/Product";
+import { IShipment } from "@/types/Shipment";
+import axios from "axios";
+
+/**
+ * Inventory Service
+ * Provide UI buisiness logic associated with product Inventory
+ */
+export class InventoryService {
+    API_URL = process.env.VUE_APP_API_URL;
+
+    public async getInventory(): Promise<any> {
+        const result = await axios.get(`${this.API_URL}/inventory`)
+        return result.data
+    }
+
+    public async updateInventoryQuantity(shipment: IShipment) {
+        let result = await axios.patch(`${this.API_URL}/inventory`, shipment)
+        return result.data
+    }
+}
