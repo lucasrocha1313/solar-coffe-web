42a26a399a18d64a0ec8d53f9a6b4081dcdb2f19
lucas rocha <lucasrocha1313@gmail.com>
2022-06-09 21:09:03 -0300

    refact with prettier

--- solarcoffer.frontend/src/components/modals/NewProductModal.vue
+++ solarcoffer.frontend/src/components/modals/NewProductModal.vue
@@ -1,102 +1,99 @@
 <template>
-    <solar-modal>
-        <template v-slot:header>
-            Add New Product
-        </template>
-        <template v-slot:body>
-            <ul class="new-product">
-                <li>
-                    <label for="is-taxable">Is this product taxable</label>
-                    <input type="checkbox" id="is-taxable" v-model="newProduct.isTaxable"/>
-                </li>
-                <li>
-                    <label for="product-name">Name</label>
-                    <input type="text" id="iproduct-name" v-model="newProduct.name"/>
-                </li>
-               <li>
-                    <label for="product-desc">Description</label>
-                    <input type="text" id="iproduct-desc" v-model="newProduct.description"/>
-                </li>
-                <li>
-                    <label for="product-price">Price (USD)</label>
-                    <input type="number" id="iproduct-price" v-model="newProduct.price"/>
-                </li>
-            </ul>
-        </template>
-        <template v-slot:footer>
-            <solar-button
-                type="button"
-                @click.native="save"
-                aria-label="save new item"
-            >
-                Save Product
-            </solar-button>
-            <solar-button
-                type="button"
-                @click.native="close"
-                aria-label="close modal"
-            >
-                Close
-            </solar-button>
-        </template>
-    </solar-modal>    
+  <solar-modal>
+    <template v-slot:header> Add New Product </template>
+    <template v-slot:body>
+      <ul class="new-product">
+        <li>
+          <label for="is-taxable">Is this product taxable</label>
+          <input
+            type="checkbox"
+            id="is-taxable"
+            v-model="newProduct.isTaxable"
+          />
+        </li>
+        <li>
+          <label for="product-name">Name</label>
+          <input type="text" id="iproduct-name" v-model="newProduct.name" />
+        </li>
+        <li>
+          <label for="product-desc">Description</label>
+          <input
+            type="text"
+            id="iproduct-desc"
+            v-model="newProduct.description"
+          />
+        </li>
+        <li>
+          <label for="product-price">Price (USD)</label>
+          <input type="number" id="iproduct-price" v-model="newProduct.price" />
+        </li>
+      </ul>
+    </template>
+    <template v-slot:footer>
+      <solar-button type="button" @click="save" aria-label="save new item">
+        Save Product
+      </solar-button>
+      <solar-button type="button" @click="close" aria-label="close modal">
+        Close
+      </solar-button>
+    </template>
+  </solar-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent } from "vue";
 import SolarButton from "../SolarButton.vue";
-import SolarModal from "@/components/modals/SolarModal.vue"
+import SolarModal from "@/components/modals/SolarModal.vue";
 
 export default defineComponent({
-    components: { SolarButton, SolarModal},
-    name: 'NewProductModal',
-    props: {},
-    methods: {
-        close() {
-            this.$emit("close")
-        },
-        save() {
-            this.$emit('save:product', this.newProduct)
-        }
+  components: { SolarButton, SolarModal },
+  name: "NewProductModal",
+  props: {},
+  methods: {
+    close() {
+      this.$emit("close");
     },
-    data() {
-        return {
-            newProduct:  {
-                id: 0,
-                createdOn: new Date(),
-                updatedOn: new Date(),
-                name: '',
-                description: '',
-                price: 0,
-                isTaxable: false,
-                isArchived: false
-            }
-        }
-    }
-})
+    save() {
+      this.$emit("save:product", this.newProduct);
+    },
+  },
+  data() {
+    return {
+      newProduct: {
+        id: 0,
+        createdOn: new Date(),
+        updatedOn: new Date(),
+        name: "",
+        description: "",
+        price: 0,
+        isTaxable: false,
+        isArchived: false,
+      },
+    };
+  },
+});
 </script>
 
 <style scoped lang="scss">
-    .new-product {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-    }
+.new-product {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
 
-    input  {
-        width: auto;
-        height: 1.8rem;
-        margin-bottom: 1.2rem;
-        font-size: 1.1rem;
-        line-height: 1.3rem;
-        padding: 0.2rem;
-        color: #555;
-    }
+input {
+  width: auto;
+  height: 1.8rem;
+  margin-bottom: 1.2rem;
+  font-size: 1.1rem;
+  line-height: 1.3rem;
+  padding: 0.2rem;
+  color: #555;
+}
 
-    label {
-        font-weight: bold;
-        display: block;
-        margin-bottom: 0.3rem;
-    }
-
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.3rem;
+}
 </style>
--- solarcoffer.frontend/src/components/modals/ShipmentModal.vue
+++ solarcoffer.frontend/src/components/modals/ShipmentModal.vue
@@ -1,82 +1,81 @@
 <template>
-    <solar-modal>
-        <template v-slot:header>
-            Receive Shipment
-        </template>
-        <template v-slot:body>
-            <label for="product">Product Received:</label>
-            <select v-model="selectedroduct" class="shipment-items" id="product">
-                <label disabled value="">Please select one</label>
-                <option v-for="item in inventory" :value="item" :key="item.product.id">
-                    {{item.product.name}}
-                </option>
-            </select>
-            <label for="qtyReceived">Quantity Received:</label>
-            <input type="number" id="qtyReceived" v-model="qtyReceived">
-        </template>
-        <template v-slot:footer>
-            <solar-button
-                type="button"
-                @button:click="save"
-                aria-label="save new shipment"> 
-                Save Received Shipment
-            </solar-button>
-            <solar-button
-                type="button"
-                @button:click="close"
-                aria-label="Close modal">
-                Close             
-            </solar-button>
-        </template>
-    </solar-modal>
+  <solar-modal>
+    <template v-slot:header> Receive Shipment </template>
+    <template v-slot:body>
+      <label for="product">Product Received:</label>
+      <select v-model="selectedroduct" class="shipment-items" id="product">
+        <label>Please select one</label>
+        <option v-for="item in inventory" :value="item" :key="item.product.id">
+          {{ item.product.name }}
+        </option>
+      </select>
+      <label for="qtyReceived">Quantity Received:</label>
+      <input type="number" id="qtyReceived" v-model="qtyReceived" />
+    </template>
+    <template v-slot:footer>
+      <solar-button
+        type="button"
+        @button:click="save"
+        aria-label="save new shipment"
+      >
+        Save Received Shipment
+      </solar-button>
+      <solar-button
+        type="button"
+        @button:click="close"
+        aria-label="Close modal"
+      >
+        Close
+      </solar-button>
+    </template>
+  </solar-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent } from "vue";
 import SolarButton from "../SolarButton.vue";
-import SolarModal from "@/components/modals/SolarModal.vue"
+import SolarModal from "@/components/modals/SolarModal.vue";
 import { IProductInventory } from "@/types/Product";
-import {IShipment} from "@/types/Shipment"
+import { IShipment } from "@/types/Shipment";
 
 export default defineComponent({
-    components: { SolarButton, SolarModal},
-    name: 'ShipmentModal',
-    props: {
-        inventory: {
-            type: Array as () => IProductInventory[],
-            required: true
-        }
+  components: { SolarButton, SolarModal },
+  name: "ShipmentModal",
+  props: {
+    inventory: {
+      type: Array as () => IProductInventory[],
+      required: true,
     },
-    methods: {
-        close() {
-            this.$emit("close")
-        },
-        save() {
-            let shipment: IShipment = {
-                productId: this.selectedroduct.id,
-                adjustment: this.qtyReceived
-            };
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    save() {
+      let shipment: IShipment = {
+        productId: this.selectedroduct.id,
+        adjustment: this.qtyReceived,
+      };
 
-            this.$emit("save:shipment", shipment);
-        }
+      this.$emit("save:shipment", shipment);
     },
-    data() {
-        return {
-            selectedroduct:  {
-                id: 0,
-                createdOn: new Date(),
-                updatedOn: new Date(),
-                name: '',
-                description: '',
-                price: 0,
-                isTaxable: false,
-                isArchived: false
-            },
-            qtyReceived: 0
-        }
-    }
-})
+  },
+  data() {
+    return {
+      selectedroduct: {
+        id: 0,
+        createdOn: new Date(),
+        updatedOn: new Date(),
+        name: "",
+        description: "",
+        price: 0,
+        isTaxable: false,
+        isArchived: false,
+      },
+      qtyReceived: 0,
+    };
+  },
+});
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
