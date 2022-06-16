153b19c3701cffd24c11c77debc25636c64211f6
lucas rocha <lucasrocha1313@gmail.com>
2022-06-16 08:11:30 -0300

    removed typing on return

--- solarcoffer.frontend/src/services/InventoryService.ts
+++ solarcoffer.frontend/src/services/InventoryService.ts
@@ -8,7 +8,7 @@ import axios from "axios";
 export class InventoryService {
   API_URL = process.env.VUE_APP_API_URL;
 
-  public async getInventory(): Promise<any> {
+  public async getInventory() {
     const result = await axios.get(`${this.API_URL}/inventory`);
     return result.data;
   }
