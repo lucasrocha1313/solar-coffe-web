f3bdcf931102a87de3bd96021e57d1423230062c
lucas rocha <lucasrocha1313@gmail.com>
2022-06-30 08:37:57 -0300

    currency mixin

---
+++ solarcoffer.frontend/src/mixins/CurrencyMixin.vue
@@ -0,0 +1,13 @@
+<script lang="js">
+export default {
+  name: "CurrencyMixin",
+  methods: {
+    currencyUSD(value) {
+      if (isNaN(value)) {
+        return "-";
+      }
+      return `$${value.toFixed(2)}`;
+    },
+  },
+};
+</script>
