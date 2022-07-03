984b4cd9abef3b4cd1e137f167b4365ab59369be
lucas rocha <lucasrocha1313@gmail.com>
2022-07-03 14:00:30 -0300

    Date mixin

---
+++ solarcoffer.frontend/src/mixins/DateMixin.vue
@@ -0,0 +1,12 @@
+<script>
+import moment from "moment";
+
+export default {
+  name: "DateMixin",
+  methods: {
+    humanizeDate(date) {
+      return moment(date).format("MMMM Do YYYY");
+    },
+  },
+};
+</script>
